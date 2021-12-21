b5ee8dbe110e1821ef7bae2e596812d69175d59d
lorenzobarracchia <lorenzobarracchia@localhost>
2021-12-21 17:59:58 +0100

    Merge branch 'master' of https://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@github.com/Simeone-L/ImmobiliareS.git

---
+++ src/main/java/it/arready/webapp/model/Indirizzo.java
@@ -0,0 +1,65 @@
+package it.arready.webapp.model;
+
+public class Indirizzo {
+	
+	private int id;
+	private String provincia;
+	private String citta;
+	private String via;
+	private int numeroCivico;
+	
+	
+	public Indirizzo() {
+		super();
+	}
+	
+	public Indirizzo(int id, String provincia, String citta, String via, int numeroCivico) {
+		super();
+		this.id = id;
+		this.provincia = provincia;
+		this.citta = citta;
+		this.via = via;
+		this.numeroCivico = numeroCivico;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getCitta() {
+		return citta;
+	}
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+	public String getVia() {
+		return via;
+	}
+	public void setVia(String via) {
+		this.via = via;
+	}
+	public int getNumeroCivico() {
+		return numeroCivico;
+	}
+	public void setNumeroCivico(int numeroCivico) {
+		this.numeroCivico = numeroCivico;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Indirizzo [id=" + id + ", provincia=" + provincia + ", citta=" + citta + ", via=" + via
+				+ ", numeroCivico=" + numeroCivico + "]";
+	}
+	
+	
+	
+
+}
