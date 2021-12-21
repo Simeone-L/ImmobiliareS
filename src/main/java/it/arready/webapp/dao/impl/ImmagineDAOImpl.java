4f0aca2578526f24cb08ce5c282c8cfe5cb3237a
Simeone <simeone@luca>
2021-12-21 17:59:55 +0100

    add Immagine(model + dao)

---
+++ src/main/java/it/arready/webapp/dao/impl/ImmagineDAOImpl.java
@@ -0,0 +1,32 @@
+package it.arready.webapp.dao.impl;
+
+import java.sql.Connection;
+import java.util.List;
+
+import it.arready.webapp.dao.DAOException;
+import it.arready.webapp.dao.ImmagineDAO;
+import it.arready.webapp.model.Immagine;
+
+public class ImmagineDAOImpl implements ImmagineDAO{
+
+	@Override
+	public void save(Connection conn, Immagine immagine) throws DAOException {
+		
+	}
+
+	@Override
+	public void update(Connection conn, Immagine immagine) throws DAOException {
+		
+	}
+
+	@Override
+	public List<Immagine> findByAll(Connection conn) throws DAOException {
+		
+	}
+
+	@Override
+	public void delete(Connection conn, Immagine immagine) throws DAOException {
+		
+	}
+
+}
--- src/main/java/it/arready/webapp/model/Model.java
+++
@@ -1,5 +0,0 @@
-package it.arready.webapp.model;
-
-public class Model {
-
-}
