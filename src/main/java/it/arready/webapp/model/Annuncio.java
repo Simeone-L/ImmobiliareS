a439ece751d28de34fd3ffa203d5bc125cf3de6b
Jules <giulianadifiore@gmail.com>
2021-12-23 14:23:50 +0100

    asdf

--- src/main/java/it/arready/webapp/model/Annuncio.java
+++ src/main/java/it/arready/webapp/model/Annuncio.java
@@ -3,25 +3,21 @@ package it.arready.webapp.model;
 import java.util.Date;
 
 public class Annuncio {
-	
+
 	private int id;
 	private Date dataAnnuncio;
-    private Immobile immobile;
+	private Immobile immobile;
 	private StatoVendita statoVendita;
 	private Utente utente;
-	
-	
-	
+
 	public enum StatoVendita {
-		AFFITTO(1, "Affitto"), AFFITTO_A_RISCATTO(2, "Affitto_riscatto"), VENDITA(3, "Vendita");
-		
+		AFFITTO(1, "Affitto"), AFFITTO_A_RISCATTO(2, "Affitto a riscatto"), VENDITA(3, "Vendita");
+
 		StatoVendita(int i, String nome) {
-			// TODO Auto-generated constructor stub
+
 		}
-	
+
 	}
-
-
 
 	public Annuncio(int id, Date dataAnnuncio, Immobile immobile, StatoVendita statoVendita, Utente utente) {
 		super();
@@ -32,66 +28,44 @@ public class Annuncio {
 		this.utente = utente;
 	}
 
-
-
 	public int getId() {
 		return id;
 	}
-
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
-
 	public Date getDataAnnuncio() {
 		return dataAnnuncio;
 	}
-
-
 
 	public void setDataAnnuncio(Date dataAnnuncio) {
 		this.dataAnnuncio = dataAnnuncio;
 	}
 
-
-
 	public Immobile getImmobile() {
 		return immobile;
 	}
-
-
 
 	public void setImmobile(Immobile immobile) {
 		this.immobile = immobile;
 	}
 
-
-
 	public StatoVendita getStatoVendita() {
 		return statoVendita;
 	}
-
-
 
 	public void setStatoVendita(StatoVendita statoVendita) {
 		this.statoVendita = statoVendita;
 	}
 
-
-
 	public Utente getUtente() {
 		return utente;
 	}
-
-
 
 	public void setUtente(Utente utente) {
 		this.utente = utente;
 	}
 
-	
-	
 }
