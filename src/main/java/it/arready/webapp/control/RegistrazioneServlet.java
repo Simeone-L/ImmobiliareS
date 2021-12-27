5752e263724b14102b7eff4759f54abf83c9a7b1
Jules <giulianadifiore@gmail.com>
2021-12-27 18:52:51 +0100

    fix

--- src/main/java/it/arready/webapp/control/RegistrazioneServlet.java
+++ src/main/java/it/arready/webapp/control/RegistrazioneServlet.java
@@ -28,39 +28,39 @@ public class RegistrazioneServlet extends HttpServlet {
 		utente.setNome("cognome");
 		utente.setEmail("email");
 		utente.setPassword("password");
-		
-		if(validate(request)) {
+
+		if (validate(request)) {
 			request.getRequestDispatcher("index.jsp").forward(request, response);
 			return;
 		}
-		
+
 		try {
 			utenteService.save(utente);
 			HttpSession session = request.getSession();
-			session.setAttribute("utente",utente);
+			session.setAttribute("utente", utente);
 			session.setAttribute("nome", utente.getNome());
 			session.setAttribute("cognome", utente.getCognome());
-			session.setAttribute("email",utente.getEmail());
+			session.setAttribute("email", utente.getEmail());
 			session.setAttribute("password", utente.getPassword());
-			
+
 			request.getRequestDispatcher("").forward(request, response);
 		} catch (ServiceException e) {
 			System.err.println(e.getMessage());
 			response.sendRedirect("");
 		}
-		
+
 		doGet(request, response);
 	}
 
 	private boolean validate(HttpServletRequest request) {
-		
+
 		String nome = request.getParameter("nome");
 		String cognome = request.getParameter("cognome");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		
+
 		boolean hasErrors = false;
-		
+
 		if (nome == null || nome.trim().isEmpty()) {
 			request.setAttribute("errorNome", "Campo Obbligatorio");
 		}
@@ -72,7 +72,6 @@ public class RegistrazioneServlet extends HttpServlet {
 		}
 		if (password == null || cognome.trim().isEmpty()) {
 			request.setAttribute("errorPassword", "Campo Obbligatorio");
-		}
 		}
 	}
 }
