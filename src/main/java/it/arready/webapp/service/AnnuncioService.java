2d6ebc3eea372723f43b47d797a29137f293a2f5
JulesFlow <96432388+JulesFlow@users.noreply.github.com>
2022-01-14 15:50:01 +0100

    add service fix
    non funzionava. scusate.

--- src/main/java/it/arready/webapp/service/AnnuncioService.java
+++ src/main/java/it/arready/webapp/service/AnnuncioService.java
@@ -1,8 +1,10 @@
 package it.arready.webapp.service;
 
+import java.sql.Connection;
 import java.util.List;
 
 import it.arready.webapp.model.Annuncio;
+import it.arready.webapp.model.Annuncio.StatoVendita;
 import it.arready.webapp.model.Immobile.StatoImmobile;
 
 public interface AnnuncioService {
@@ -10,14 +12,16 @@ public interface AnnuncioService {
 	void save(Annuncio annuncio) throws ServiceException;
 
 	void update(Annuncio annuncio) throws ServiceException;
-	
+
 	void delete(Annuncio annuncio) throws ServiceException;
 
 	Annuncio findById(int id) throws ServiceException;
 
 	Annuncio findByTitolo(String titolo) throws ServiceException;
 
-	List<Annuncio> orderByFind(Float prezzoMin, Float prezzoMax, Integer numLocali, Integer numBagni, Float superficie,
-			Integer piano, StatoImmobile statoImmobile, String citta, String titolo) throws ServiceException;
+	List<Annuncio> orderByFind(Float prezzoMin, Float prezzoMax, Integer numLocali, Integer numBagni,
+			Float superficieMin, Float superficieMax, Integer piano, StatoImmobile statoImmobile, String citta,
+			String titolo, String provincia, String indirizzo, Integer numeroCivico, StatoVendita statoVendita)
+			throws ServiceException;
 
 }
