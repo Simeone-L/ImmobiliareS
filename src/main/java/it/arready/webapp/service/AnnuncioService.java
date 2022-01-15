9ef78ddd6ba2d574d15e4943570e140e76d20f82
JulesFlow <96432388+JulesFlow@users.noreply.github.com>
2022-01-15 17:23:31 +0100

    AnnuncioService update

--- src/main/java/it/arready/webapp/service/AnnuncioService.java
+++ src/main/java/it/arready/webapp/service/AnnuncioService.java
@@ -1,9 +1,9 @@
 package it.arready.webapp.service;
 
-import java.sql.Connection;
 import java.util.List;
 
 import it.arready.webapp.model.Annuncio;
+import it.arready.webapp.model.Annuncio.Ordinamento;
 import it.arready.webapp.model.Annuncio.StatoVendita;
 import it.arready.webapp.model.Immobile.StatoImmobile;
 
@@ -21,7 +21,7 @@ public interface AnnuncioService {
 
 	List<Annuncio> orderByFind(Float prezzoMin, Float prezzoMax, Integer numLocali, Integer numBagni,
 			Float superficieMin, Float superficieMax, Integer piano, StatoImmobile statoImmobile, String citta,
-			String titolo, String provincia, String indirizzo, Integer numeroCivico, StatoVendita statoVendita)
-			throws ServiceException;
+			String titolo, String provincia, String indirizzo, Integer numeroCivico, StatoVendita statoVendita,
+			Ordinamento ordinamentoPrezzo, Ordinamento ordinamentoSuperficie) throws ServiceException;
 
 }
